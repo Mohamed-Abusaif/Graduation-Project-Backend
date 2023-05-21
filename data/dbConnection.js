c3fcc4ac3d218db6a6ff97d23cc46334e7fe2022
Mohamed Abusaif <67015050+Mohamed-Abusaif@users.noreply.github.com>
2023-05-21 23:47:28 +0300

    commit

--- data/dbConnection.js
+++ data/dbConnection.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 
 const pool = mysql.createPool({
   connectionLimit: 100, //important
-  host: "209.38.194.100",
+  host: "localhost",
   user: "root",
   password: "root",
   database: "courseschemagp",
