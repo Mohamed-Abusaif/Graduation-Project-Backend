0ec132943f053ea1a372fa2e2f0f9fe81c65b0b1
Mohamed Abusaif <67015050+Mohamed-Abusaif@users.noreply.github.com>
2023-05-23 01:47:33 +0300

    conn err

--- data/dbConnection.js
+++ data/dbConnection.js
@@ -2,6 +2,7 @@ const mysql = require("mysql2");
 
 const pool = mysql.createPool({
   connectionLimit: 100, //important
+  port: '/var/run/mysqld/mysqld.sock',
   host: "localhost",
   user: "root",
   password: "root",
